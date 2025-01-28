035357c931ad335062a0d4e4817b43a742822ec4
Luiz Vasconcellos <luizpvasc@gmail.com>
2025-01-28 15:11:16 -0300

    restful endpoint

---
+++ app/controllers/combo/components/actions/action_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Combo::Components::Actions::ExecutionsController < Combo::ApplicationController
+  def create
+    raise "not implemented"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 Combo::Engine.routes.draw do
+  post "/components/actions/executions" => "components/actions/executions#create",
+    name: :create_component_action_execution
 end
